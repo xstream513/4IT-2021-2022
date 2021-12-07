ef19b0cf72c37ee2c592a373eff4d17856383c1f
Jan Sýkora <jan.sykora@datalite.cz>
2021-12-07 11:01:33 +0100

    procvičování funkcí - validace IPv4 adresy, převod z dvojkové soustavy

---
+++ src/ArrayTraining.java
@@ -0,0 +1,55 @@
+public class ArrayTraining {
+
+    public static void main(String[] args) {
+        // 192.168.45.80
+        validateIPv4("192.168.45.80");
+        decimalToBinary(5);
+
+        // zkuste si doma udělat funkci binaryToDecimal, která převede číslo ve dvojkové soustavě na desítkovou
+    }
+
+    public static String decimalToBinary(int input) {
+//        String inputString = String.valueOf(input);
+        int mezikrok = input;
+        String vysledek = "";
+        while(mezikrok != 0) {
+            if (mezikrok % 2 == 0) {
+                vysledek = vysledek + "0";
+                mezikrok = mezikrok / 2;
+            } else {
+                vysledek = vysledek + "1";
+                mezikrok = mezikrok / 2;
+            }
+        }
+        return vysledek;
+    }
+
+    public static boolean validateIPv4(String input) {
+        String[] inputSplit = input.split("\\.");
+        if (inputSplit.length != 4) {
+            return false;
+        }
+        for (String element : inputSplit) {
+            if (isParsableInteger(element)) {
+                Integer elementInteger = Integer.parseInt(element);
+                if (elementInteger < 0 || elementInteger > 255) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isParsableInteger(String input) {
+        try {
+            Integer intValue = Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+}
